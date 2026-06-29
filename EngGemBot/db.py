bcd48933d7844c4c34d7006bf1ad542fbec2ae76
KirilKuprum <kirilshev9@gmail.com>
2026-06-29 18:42:05 +0300

    Виправлено структуру папки EngGemBot

---
+++ EngGemBot/db.py
@@ -0,0 +1,29 @@
+import datetime
+
+import boto3
+
+
+class DataBase:
+    def __init__(self, table_name, region):
+        self.dynamodb = boto3.resource(
+            'dynamodb',
+            region_name=region)
+        self.table = self.dynamodb.Table(table_name)
+
+    def __str__(self):
+        return str(self.table)
+
+    def put_item(self, item: dict):
+        item['created_at'] = str(datetime.datetime.now())
+        self.table.put_item(
+            Item=item
+        )
+    def get_item(self, user_id: int) -> dict | None:
+        try:
+            response = self.table.get_item(
+                Key={'user_id': str(user_id)} 
+            )
+            return response.get('Item')
+        except ClientError as e:
+            print(f"Помилка отримання даних з DynamoDB: {e.response['Error']['Message']}")
+            return None

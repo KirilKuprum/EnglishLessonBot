62f7271cf1f8377b47f776d4908842e7e6d3539e
KirilKuprum <154259918+KirilKuprum@users.noreply.github.com>
2026-06-30 19:36:18 +0300

    Update bot.py

--- EngGemBot_new/bot.py
+++ EngGemBot_new/bot.py
@@ -66,7 +66,7 @@ async def cmd_encard(message: Message):
     try:
         prompt = PromptBuilder.simplePrompt()
         response = client.models.generate_content(
-            model="gemini-1.5-flash",
+            model="models/gemini-1.5-flash",
             contents=prompt
         )
         match = re.search(r'\[.*\]', response.text, re.DOTALL)
